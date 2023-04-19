951f5a22956bce3e5d3add13a35c77f351c49dd9
Torishnia <valeri.torishnya@gmail.com>
2023-04-19 18:55:55 +0300

    Fixing the CORS error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import multer from 'multer';
+import cors from 'cors';
 
 import { registerValidation, loginValidation, postCreateValidation } from './validations/validations.js';
 import { handleValidationErrors, checkAuth } from './utils/index.js';
@@ -26,6 +27,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
